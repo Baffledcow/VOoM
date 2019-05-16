1318df8092e878962f3058eef0817cc9857817f1
Jon Leack <jon.leack@gmail.com>
2019-05-16 10:21:35 +0100

    feat: add `golang` mode for Go using `func` as level 1 heading marker

---
+++ autoload/voom/voom_vimplugin2657/voom_mode_golang.py
@@ -0,0 +1,62 @@
+# File: voom_mode_hashes.py
+# Last Modified: 2017-01-07
+# Description: VOoM -- two-pane outliner plugin for Python-enabled Vim
+# Website: http://www.vim.org/scripts/script.php?script_id=2657
+# Author: Vlad Irnov (vlad DOT irnov AT gmail DOT com)
+# License: CC0, see http://creativecommons.org/publicdomain/zero/1.0/
+
+"""
+VOoM markup mode for Golang
+
+Features:
+
+* Interprets `func foo() {` as a level-one heading.
+
+Limitations:
+
+* Does not support level changes.
+"""
+
+import sys
+if sys.version_info[0] > 2:
+        xrange = range
+
+import re
+
+headline_match = re.compile(r'^func ([^(]+)').match
+
+def hook_makeOutline(VO, blines):
+    """Return (tlines, bnodes, levels) for Body lines blines.
+    blines is either Vim buffer object (Body) or list of buffer lines.
+    """
+    Z = len(blines)
+    tlines, bnodes, levels = [], [], []
+    tlines_add, bnodes_add, levels_add = tlines.append, bnodes.append, levels.append
+    for i in xrange(Z):
+        bline = blines[i]
+        m = headline_match(bline)
+        if not m:
+            continue
+        lev = 1
+        head = m.groups()[0].strip()
+        tline = '  %s|%s' %('. '*(lev-1), head)
+        tlines_add(tline)
+        bnodes_add(i+1)
+        levels_add(lev)
+    return (tlines, bnodes, levels)
+
+
+def hook_newHeadline(VO, level, blnum, tlnum):
+    """Return (tree_head, bodyLines).
+    tree_head is new headline string in Tree buffer (text after |).
+    bodyLines is list of lines to insert in Body buffer.
+    """
+    tree_head = 'NewHeadline'
+    bodyLines = ['func newFunc() {', '}', '']
+    return (tree_head, bodyLines)
+
+
+def hook_changeLevBodyHead(VO, h, levDelta):
+    """Increase of decrease level number of Body headline by levDelta."""
+    # We don't support level changes for this file type.
+    return h
